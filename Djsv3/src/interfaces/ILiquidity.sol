e0af4d29c9f2cb4869f94b3d1d1658be4af0197b
Keccak Edwin <edwin@KeccakdeMacBook-Air.local>
2026-01-04 19:48:26 +0800

    delete old version Djsv3

--- Djsv3/src/interfaces/ILiquidity.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-interface ILiquidity{
-    function swapForSubTokenToUser(address to, uint256 amountUSDT) external;
-    function swapForSubTokenToBurn(uint256 amountUSDT) external;
-    function addLiquidity(uint256 amountUSDT) external;
-    function acquireSpecifiedUsdt(address to, uint256 amountUSDT) external;
-}
